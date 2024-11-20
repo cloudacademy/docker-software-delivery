f202c366ffeb0adf37a7f301e7b426e66231c28a
Naz Rejepov <nazar.rejepov@qa.com>
2024-11-20 12:40:35 +0000

    update intergration.sh

--- src/integration.sh
+++ src/integration.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+echo "Begin testing..."
+sleep 3
+
 host=localhost
 port=80
 
+# Check if there are existing messages
 messages_response=`curl -s $host:$port/api`
 if [[ $messages_response != '[]' ]]; then
-    >&2 echo Unexpected inital messages response: $messages_response
-    exit 1
+    echo "Messages already exist in the database. Skipping initial message response test."
+else
+    if [[ $messages_response != '[]' ]]; then
+        >&2 echo Unexpected initial messages response: $messages_response
+        exit 1
+    fi
+    echo Passed initial message response test
 fi
-echo Passed initial message response test
 
 post_response=`curl -s -XPOST -H "Content-Type: application/json" -d '{"message":"test"}' $host:$port/api`
 if [[ $post_response != *"test"* || $post_response != *"_id"* ]]; then
